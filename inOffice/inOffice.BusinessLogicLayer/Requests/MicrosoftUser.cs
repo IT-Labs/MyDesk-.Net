132cadf2deb9844519f52076db09affd4d2a0215
Neven Eric <neven.eric@it-labs.com>
2022-08-18 12:40:54 +0200

    Password on DTO marked as not-required

--- inOffice/inOffice.BusinessLogicLayer/Requests/MicrosoftUser.cs
+++ inOffice/inOffice.BusinessLogicLayer/Requests/MicrosoftUser.cs
@@ -6,7 +6,7 @@
         public string Firstname { get; set; }
         public string Surname { get; set; }
         public string JobTitle { get; set; }
-        public string Password { get; set; }
+        public string? Password { get; set; }
         public bool IsAdmin { get; set; }
     }
 }
