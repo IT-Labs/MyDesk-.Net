66e64c3c2649585e6c9273cdc865d218daeb65db
Neven Eric <neven.eric@it-labs.com>
2022-08-26 14:34:31 +0200

    Bug 47479: Grammar mistake fixed

--- inOffice/inOffice.BusinessLogicLayer/Responses/ReservationResponse.cs
+++ inOffice/inOffice.BusinessLogicLayer/Responses/ReservationResponse.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace inOffice.BusinessLogicLayer.Responses
+﻿namespace inOffice.BusinessLogicLayer.Responses
 {
     public class ReservationResponse
     {
         public bool Success { get; set; }
+        public string ErrorMessage { get; set; }
     }
 }
