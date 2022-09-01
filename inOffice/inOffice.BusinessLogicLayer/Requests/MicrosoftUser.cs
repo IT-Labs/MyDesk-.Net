9a45e061e09bd8fde0386f2d153e8ab3ed1f3722
Neven Eric <neven.eric@it-labs.com>
2022-09-01 15:01:51 +0200

    Removed required fields from DTO

--- inOffice/inOffice.BusinessLogicLayer/Requests/MicrosoftUser.cs
+++ inOffice/inOffice.BusinessLogicLayer/Requests/MicrosoftUser.cs
@@ -3,10 +3,10 @@
     public class MicrosoftUser
     {
         public string Email { get; set; }
-        public string Firstname { get; set; }
-        public string Surname { get; set; }
-        public string JobTitle { get; set; }
+        public string? Firstname { get; set; }
+        public string? Surname { get; set; }
+        public string? JobTitle { get; set; }
         public string? Password { get; set; }
-        public bool IsAdmin { get; set; }
+        public bool? IsAdmin { get; set; }
     }
 }
